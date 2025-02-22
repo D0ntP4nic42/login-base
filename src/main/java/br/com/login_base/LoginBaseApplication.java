7260e475b677ec11adb42677e5af40c3efedd002
D0ntP4nic <leozinngomes@gmail.com>
2025-02-22 17:37:23 -0300

    initial commit

---
+++ src/main/java/br/com/login_base/LoginBaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.login_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/login_base/LoginBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.login_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
